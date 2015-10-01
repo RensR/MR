0a87e32186240bd7117ae49f1da4360d6a384671
Armen <armenshamelian@outlook.com>
2015-10-01 10:39:11 +0200

    commit

--- MagicalMusic/public/javascripts/music.js
+++ MagicalMusic/public/javascripts/music.js
@@ -2,7 +2,7 @@ $(function() {
     // add a click handler to the button
     $("#submitMood").click(function(event) {
         // make an ajax get request to get the message
-        alert("Succes!");
+       // alert("Succes!");
         jsRoutes.controllers.MessageController.getMessage().ajax({
             success: function(data) {
                 $(".wells").append($("<h1>").text(data.value));
